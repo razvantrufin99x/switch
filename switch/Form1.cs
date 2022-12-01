8c5c01491a4a3805168cab346355bd158e1c0691
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-02 00:15:08 +0200

    Add project files.

---
+++ switch/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace @switch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void schimba(ref string x, ref string y)
+        {
+            string tmp;
+            tmp = x;
+            x = y ;
+            y = tmp;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            string b = textBox2.Text;
+
+            schimba(ref a, ref b );
+            textBox1.Text = a;
+            textBox2.Text = b;
+        }
+    }
+}
